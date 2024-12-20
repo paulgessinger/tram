39cc1407453480866a4dd2f818ab44a023f21cf0
Paul Gessinger <hello@paulgessinger.com>
2024-12-20 14:59:02 +0100

    update to ojp

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import requests
 import pydantic
 from dotenv import load_dotenv
 import xml.etree.ElementTree as ET
-from flask import Flask, request,jsonify
+from flask import Flask, request, jsonify
 
 load_dotenv()
 
@@ -22,38 +22,41 @@ class Departure(pydantic.BaseModel):
 class Response(pydantic.BaseModel):
     departures: list[Departure] = pydantic.Field(default_factory=list)
 
+
 def get_departures(requested_lines: list[str]) -> list[Departure]:
-    url = "https://api.opentransportdata.swiss/trias2020"
+    url = "https://api.opentransportdata.swiss/ojp2020"
 
     request = """
-    <?xml version="1.0" encoding="UTF-8"?>
-    <Trias version="1.1" xmlns="http://www.vdv.de/trias" xmlns:siri="http://www.siri.org.uk/siri" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<?xml version="1.0" encoding="UTF-8"?>
+<OJP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="http://www.siri.org.uk/siri" version="1.0" xmlns:ojp="http://www.vdv.de/ojp" xsi:schemaLocation="http://www.siri.org.uk/siri ../ojp-xsd-v1.0/OJP.xsd">
+    <OJPRequest>
         <ServiceRequest>
-            <siri:RequestTimestamp>2023-11-03T08:38:09.013Z</siri:RequestTimestamp>
-            <siri:RequestorRef>API-Explorer</siri:RequestorRef>
-            <RequestPayload>
-                <StopEventRequest>
-                    <Location>
-                        <LocationRef>
-                            <StopPointRef>8592922</StopPointRef>
-                        </LocationRef>
-                        <DepArrTime>{time:%Y-%m-%dT%H:%M:%S}</DepArrTime>
-                    </Location>
-                    <Params>
-                        <NumberOfResults>40</NumberOfResults>
-                        <StopEventType>departure</StopEventType>
-                        <IncludePreviousCalls>false</IncludePreviousCalls>
-                        <IncludeOnwardCalls>false</IncludeOnwardCalls>
-                        <IncludeRealtimeData>true</IncludeRealtimeData>
-                    </Params>
-                </StopEventRequest>
-            </RequestPayload>
+            <RequestTimestamp>2024-12-20T13:44:03.796Z</RequestTimestamp>
+            <RequestorRef>API-Explorer</RequestorRef>
+            <ojp:OJPStopEventRequest>
+                <RequestTimestamp>2024-12-20T13:44:03.796Z</RequestTimestamp>
+                <ojp:Location>
+                    <ojp:PlaceRef>
+                        <StopPlaceRef>8592922</StopPlaceRef>
+                        <ojp:LocationName>
+                            <ojp:Text>Genève, Vieusseux</ojp:Text>
+                        </ojp:LocationName>
+                    </ojp:PlaceRef>
+                    <ojp:DepArrTime>{time:%Y-%m-%dT%H:%M:%S}</ojp:DepArrTime>
+                </ojp:Location>
+                <ojp:Params>
+                    <ojp:NumberOfResults>40</ojp:NumberOfResults>
+                    <ojp:StopEventType>departure</ojp:StopEventType>
+                    <ojp:IncludeRealtimeData>true</ojp:IncludeRealtimeData>
+                </ojp:Params>
+            </ojp:OJPStopEventRequest>
         </ServiceRequest>
-    </Trias>
+    </OJPRequest>
+</OJP>
     """.strip()
 
     request = request.format(time=datetime.datetime.now())
-    token = os.environ.get("API_KEY")
+    token = os.environ["API_KEY"]
     r = requests.request(
         method="post",
         url=url,
@@ -67,28 +70,24 @@ def get_departures(requested_lines: list[str]) -> list[Departure]:
 
     namespaces = {
         "siri": "http://www.siri.org.uk/siri",
-        "trias": "http://www.vdv.de/trias",
-        "acsb": "http://www.ifopt.org.uk/acsb",
-        "ifopt": "http://www.ifopt.org.uk/ifopt",
-        "datex2": "http://datex2.eu/schema/1_0/1_0",
+        "ojp": "http://www.vdv.de/ojp",
     }
 
     tree = ET.fromstring(r.text)
 
-    requested_lines = {"18"}
-
+    requested_lines = ["18"]
 
     departures = []
 
-    for stop_event in tree.findall(".//trias:StopEvent", namespaces=namespaces):
-        this_call = stop_event.find("trias:ThisCall", namespaces=namespaces)
+    for stop_event in tree.findall(".//ojp:StopEvent", namespaces=namespaces):
+        this_call = stop_event.find("ojp:ThisCall", namespaces=namespaces)
         assert this_call is not None
-        service = stop_event.find("trias:Service", namespaces=namespaces)
+        service = stop_event.find("ojp:Service", namespaces=namespaces)
         assert service is not None
         #  print(this_call, service)
 
         published_line_name = service.find(
-            "trias:PublishedLineName/trias:Text", namespaces=namespaces
+            "ojp:PublishedLineName/ojp:Text", namespaces=namespaces
         )
         assert published_line_name is not None
         published_line_name = published_line_name.text
@@ -97,7 +96,7 @@ def get_departures(requested_lines: list[str]) -> list[Departure]:
             continue
 
         destination_text = service.find(
-            "trias:DestinationText/trias:Text", namespaces=namespaces
+            "ojp:DestinationText/ojp:Text", namespaces=namespaces
         )
         assert destination_text is not None
         destination_text = destination_text.text
@@ -108,7 +107,7 @@ def get_departures(requested_lines: list[str]) -> list[Departure]:
         #  print(published_line_name, "->", destination_text)
 
         service_departure_timetabled = this_call.find(
-            "trias:CallAtStop/trias:ServiceDeparture/trias:TimetabledTime",
+            "ojp:CallAtStop/ojp:ServiceDeparture/ojp:TimetabledTime",
             namespaces=namespaces,
         )
         assert service_departure_timetabled is not None, "Node not found"
@@ -116,7 +115,7 @@ def get_departures(requested_lines: list[str]) -> list[Departure]:
         assert service_departure_timetabled is not None, "Text not found"
 
         service_departure_estimated = this_call.find(
-            "trias:CallAtStop/trias:ServiceDeparture/trias:EstimatedTime",
+            "ojp:CallAtStop/ojp:ServiceDeparture/ojp:EstimatedTime",
             namespaces=namespaces,
         )
 
@@ -153,10 +152,16 @@ def get_departures(requested_lines: list[str]) -> list[Departure]:
 
 app = Flask(__name__)
 
+
 @app.get("/")
 def load_departures():
     departures = get_departures(["18"])
-    return Response(departures=departures).json(), 200, {"Content-Type": "application/json"}
+    return (
+        Response(departures=departures).model_dump_json(),
+        200,
+        {"Content-Type": "application/json"},
+    )
+
 
 @app.get("/as-text")
 def departures_as_text():
@@ -172,19 +177,16 @@ def departures_as_text():
 
     frags = []
 
-    for  dep in departures[:num]:
+    for dep in departures[:num]:
         best_time = dep.estimated or dep.timetabled
         delta = best_time - datetime.datetime.utcnow()
-        minutes = math.floor(delta.total_seconds()/60)
+        minutes = math.floor(delta.total_seconds() / 60)
         if minutes == 1:
             frags.append("einer Minute")
         else:
             frags.append(f"{minutes} Minuten")
 
-
     times = ", ".join(frags[:-1]) + " und " + frags[-1]
-    text +=  " " + times
+    text += " " + times
 
     return jsonify({"text": text})
-
-
